80fcf34d3fec379a0c14d2d18f64c71b9a6f5a12
William Ku <kucheech@gmail.com>
2018-06-05 21:42:22 +0800

    Initial commit

---
+++ app/src/main/java/sg/com/kaplan/spinnerexample/MainActivity.java
@@ -0,0 +1,37 @@
+package sg.com.kaplan.spinnerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Button button = (Button) findViewById(R.id.button);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String planet = spinner.getSelectedItem().toString();
+                Toast.makeText(MainActivity.this, planet, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
